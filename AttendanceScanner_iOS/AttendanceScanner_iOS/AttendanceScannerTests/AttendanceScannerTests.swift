00633302722da02809d6eb5d2ed231b2f08869e1
Ioana <61358172+ioa7a@users.noreply.github.com>
2024-12-10 22:13:43 +0200

    Update iOS app
    - write attendance to database
    - mock student username

---
+++ AttendanceScanner_iOS/AttendanceScanner_iOS/AttendanceScannerTests/AttendanceScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceScannerTests.swift
+//  AttendanceScannerTests
+//
+//  Created by Ioana Bojinca on 15.11.2024.
+//
+
+import Testing
+@testable import AttendanceScanner
+
+struct AttendanceScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
